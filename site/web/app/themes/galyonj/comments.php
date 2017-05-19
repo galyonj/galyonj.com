2415dfcaf5fc011489e622b8e660932588f1e388
John Galyon <jgalyon@covhlth.co>
2017-05-18 20:15:06 -0400

    Comments template is ready to deploy. I hope I don't regret this.

--- site/web/app/themes/galyonj/comments.php
+++ site/web/app/themes/galyonj/comments.php
@@ -20,7 +20,9 @@ if ( post_password_required() ) {
 ?>
 
 <div id="comments" class="comment-area">
-	<?php if( have_comments() ) : ?>
+	<?php
+	// You can start editing here -- including this comment!
+	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
 			$comments_number = get_comments_number();
@@ -35,7 +37,7 @@ if ( post_password_required() ) {
 						'%1$s Replies to &ldquo;%2$s&rdquo;',
 						$comments_number,
 						'comments title',
-						'galyonj'
+						'twentyseventeen'
 					),
 					number_format_i18n( $comments_number ),
 					get_the_title()
@@ -43,5 +45,55 @@ if ( post_password_required() ) {
 			}
 			?>
 		</h2>
-	<?php endif; ?>
+
+		<ol class="comment-list">
+			<?php
+			wp_list_comments( array(
+				'avatar_size' => 100,
+				'style'       => 'ol',
+				'short_ping'  => true,
+				'reply_text'  => '<i class="fa fa-reply" aria-hidden="true"></i> ' . __( 'Reply', 'galyonj' ),
+			) );
+			?>
+		</ol>
+
+		<?php the_comments_pagination( array(
+			'prev_text' => '<i class="fa fa-chevron-left" aria-hidden="true"></i><span class="screen-reader-text">' . __( 'Previous', 'galyonj' ) . '</span>',
+			'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'galyonj' ) . '</span><i class="fa fa-chevron-right" aria-hidden="true"></i>',
+		) );
+
+	endif; // Check for have_comments().
+
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'galyonj' ); ?></p>
+		<?php
+	endif;
+
+	$commenter = wp_get_current_commenter();
+	$req = get_option( 'require_name_email' );
+	$aria_req = ( $req ? ' aria-required="true"' : '' );
+
+	$fields = array(
+		'author' => '<div class="form-group"><label for="author">' . __( 'Name', 'galyonj') . '</label>' . ( $req ? '<span class="required">*</span>' : '' ) . '<input id="author" class="form-control" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" ' . ( $req ? 'aria-required="true"' : '' ) . '></div>',
+		'email'  => '<div class="form-group"><label for="email">' . __( 'Email', 'galyonj') . '</label>' . ( $req ? '<span class="required">*</span>' : '' ) . '<input id="email" class="form-control" name="email" type="text" value="' . esc_attr( $commenter['comment_author_email'] ) . '" ' . ( $req ? 'aria-required="true"' : '' ) . '></div>',
+		'url'    => '<div class="form-group"><label for="url">' . __( 'Website', 'galyonj') . '</label><input id="url" class="form-control" name="url" type="url" value="' . esc_attr( $commenter['comment_author_url'] ) . '" ' . ( $req ? 'aria-required="true"' : '' ) . '></div>'
+	);
+
+	$comment_args = array(
+		'comment_field' => '<div class="form-group"><label for="comment">' . __( 'Comment', 'galyonj' ) . '</label>'
+		                   . ( $req ? '<span class="required">*</span>' : '' ) . '<textarea id="comment" name="comment" class="form-control" rows="8" ' . $aria_req . '></textarea></div>',
+		'fields'        => apply_filters( 'comment_form_default_fields', $fields ),
+		'submit_button' => '<input name="%1$s" type="submit" id="%2$s" class="%3$s btn btn-default" value="%4$s" />',
+		'title_reply'   => __( 'Leave a reply', 'galyonj' ),
+	);
+
+	comment_form( $comment_args );
+	?>
 </div>
+
+<!--<div class="form-group">
+	<label for="author"><?php /*__( 'Name', 'galyonj' ); ( $req ? '<span class="required">*</span>' : '' ); */?></label>
+	<input type="text" id="author" class="form-control" name="author" <?php /*$aria_req; */?> value="<?php /*esc_attr( $commenter[comment_author] ); */?>">
+</div>-->
